fe8dc780fc6b93dd5f82feb2c23f9ba1c29c645b
igory67 <serveruser1@mydomain.local>
2025-11-10 11:12:09 +0000

    logs final

--- 5_drawing_board/8_writable_logs.sh
+++ 5_drawing_board/8_writable_logs.sh
@@ -2,7 +2,7 @@
 
 print_2title "Log files with weird permissions (up to 50)"
 find /var/log -type f -ls 2>/dev/null | \
-grep -Ev "root\s+root|root\s+systemd-journal|root\s+syslog|root\s+utmp|root\s+adm|www-data\s+root" | \
+grep -Ev "root\s+root|root\s+systemd-journal|root\s+syslog|root\s+utmp|root\s+adm|www-data\s+root|syslog\s+adm" | \
 sed -${E} "s,.*,${SED_RED},g" | \
 head -n 50
 
