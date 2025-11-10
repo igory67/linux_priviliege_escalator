ed920745fc681be4ee07f82c462b061ee659c576
masik <etoyadadaa@gmail.com>
2025-11-10 17:16:46 +0300

    reverted history

--- 2_files_checks/5_drawing_board/16_password_in_history.sh
+++ 2_files_checks/5_drawing_board/16_password_in_history.sh
@@ -5,13 +5,13 @@ print_2title "password in cmd history"
 # print_2title "passwords in history files"
 
 for histfile in "$HOME/.bash_history" "$HOME/.zsh_history" "$HOME/.ash_history" "$HOME/.histfile"; do
-    [ -f "$histfile" ] && grep -Ei "$pwd_inside_history" 2>/dev/null | sed -${E} "s,$pwd_inside_history,${SED_RED},"
+    [ -f "$histfile" ] && grep -EiH "$pwd_inside_history" "$histfile" 2>/dev/null | sed -${E} "s,$pwd_inside_history,${SED_RED},"
 done
 
 # Search root's history files if we're root
 if [ "$IAMROOT" -eq 1 ]; then
     for histfile in "/root/.bash_history" "/root/.zsh_history" "/root/.ash_history" "/root/.histfile"; do
-        [ -f "$histfile" ] && grep -Ei "$pwd_inside_history" 2>/dev/null | sed -${E} "s,$pwd_inside_history,${SED_RED},"
+        [ -f "$histfile" ] && grep -EiH "$pwd_inside_history" "$histfile" 2>/dev/null | sed -${E} "s,$pwd_inside_history,${SED_RED},"
     done
 fi
 
