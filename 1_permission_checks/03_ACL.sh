d90bf8d2edc8adcbd8e9d5df3a385cda65298c83
masik <etoyadadaa@gmail.com>
2025-11-14 17:08:59 +0300

    04 caps

--- 1_permission_checks/03_ACL.sh
+++ 1_permission_checks/03_ACL.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-print_2title "ACL files up to 50"
+print_2title "ACL files up to 70"
 ( (getfacl -t -s -R -p /bin /etc $HOMESEARCH /opt /sbin /usr /tmp /root 2>/dev/null) \
 || echo_not_found "files with acls in searched folders" ) \
 | head -n 70 | sed -${E} "s,$sh_usrs,${SED_LIGHT_CYAN}," \
---
+++ 1_permission_checks/04_capabilities.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+print_2title " Capabilities! "
+#чо за ебучий синтаксис какой-то, чекнуть что в ифе, почему через или; command -v capsh это вот так мы будем с ним общаться о Боги
+if [ "$(command -v capsh || echo -n '')" ]; then
+
+    print_green "CURRENT SHELL CAPABILITIES "
+    cat "/proc/$$/status" | grep Cap | while read -r cap_line; do
+    
+    cap_name=$(echo "$cap_line" | awk '{print $1}') #name 
+    
+    cap_value=$(echo "$cap_line" | awk '{print $2}') #00000
+
+    if [ "$cap_name" = "CapEff:" ]; then
+        
+        if [[ "$cap_value" =~ ^[0-9a-fA-F]+$ ]]; then
+            echo "$cap_name	 $(capsh --decode=0x"$cap_value" 2>/dev/null | sed -${E} "s,$capsB,${SED_RED_YELLOW},")"
+        else
+            echo "$cap_name	 Not in hash format! for some reason? "
+        fi
+        
+        else
+
+        if [[ "$cap_value" =~ ^[0-9a-fA-F]+$ ]]; then
+            echo "$cap_name  $(capsh --decode=0x"$cap_value" 2>/dev/null | sed -${E} "s,$capsB,${SED_RED},")"
+        else
+            echo "$cap_name  [Invalid capability format]"
+        fi
+
+    fi
+done
