1dad334fed7aff32d1ae675f01376ac572beabf5
masik <etoyadadaa@gmail.com>
2025-11-14 17:11:41 +0300

    04 caps

--- 1_permission_checks/04_capabilities.sh
+++ 1_permission_checks/04_capabilities.sh
@@ -5,27 +5,29 @@ print_2title " Capabilities! "
 if [ "$(command -v capsh || echo -n '')" ]; then
 
     print_green "CURRENT SHELL CAPABILITIES "
-    cat "/proc/$$/status" | grep Cap | while read -r cap_line; do
+    cat "/proc/$$/status" | grep Cap \
+    | while read -r cap_line; do
     
-    cap_name=$(echo "$cap_line" | awk '{print $1}') #name 
+        cap_name=$(echo "$cap_line" | awk '{print $1}') #name 
     
-    cap_value=$(echo "$cap_line" | awk '{print $2}') #00000
+        cap_value=$(echo "$cap_line" | awk '{print $2}') #00000
 
-    if [ "$cap_name" = "CapEff:" ]; then
-        
-        if [[ "$cap_value" =~ ^[0-9a-fA-F]+$ ]]; then
-            echo "$cap_name	 $(capsh --decode=0x"$cap_value" 2>/dev/null | sed -${E} "s,$capsB,${SED_RED_YELLOW},")"
-        else
-            echo "$cap_name	 Not in hash format! for some reason? "
+        if [ "$cap_name" = "CapEff:" ]; then
+            
+            if [[ "$cap_value" =~ ^[0-9a-fA-F]+$ ]]; then
+                echo "$cap_name	 $(capsh --decode=0x"$cap_value" 2>/dev/null | sed -${E} "s,$capsB,${SED_RED_YELLOW},")"
+            else
+                echo "$cap_name	 Not in hash format! for some reason? "
+            fi
+            
+            else
+
+            if [[ "$cap_value" =~ ^[0-9a-fA-F]+$ ]]; then
+                echo "$cap_name  $(capsh --decode=0x"$cap_value" 2>/dev/null | sed -${E} "s,$capsB,${SED_RED},")"
+            else
+                echo "$cap_name  [Invalid capability format]"
+            fi
+
         fi
-        
-        else
-
-        if [[ "$cap_value" =~ ^[0-9a-fA-F]+$ ]]; then
-            echo "$cap_name  $(capsh --decode=0x"$cap_value" 2>/dev/null | sed -${E} "s,$capsB,${SED_RED},")"
-        else
-            echo "$cap_name  [Invalid capability format]"
-        fi
-
-    fi
-done
+    done
+fi
