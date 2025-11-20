7b926de27763a45ab7bf3d0eab3a08e17abf4a51
masik <etoyadadaa@gmail.com>
2025-11-20 09:42:15 +0300

    new file new global

---
+++ 1_permission_checks/08_scripts_in_profile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+print_2title "Scripts in /etc/profile.d/" #""The file /etc/profile and the files under /etc/profile.d/ are scripts that are
+# executed when a user runs a new shell. Therefore, if you can write or modify any of them you can escalate privileges.""
+
+if ! [ "$IAMROOT" ]; then 
+  (ls -la /etc/profile.d/ 2>/dev/null | sed -${E} "s,$profiledG,${SED_GREEN},") || echo_not_found "/etc/profile.d/"
+  check_critial_root_path "/etc/profile"
+  check_critial_root_path "/etc/profile.d/"
+fi
+  
+
+echo ""
+
--- global.sh
+++ global.sh
@@ -36,6 +36,7 @@ print_green()
     printf   "${PRINTF_BOLD_GREEN}%s${NC}\n" "$1"
 }
 
+
 print_blue()
 {
 	printf   "${PRINTF_BOLD_BLUE}%s${NC}\n" "$1"
@@ -140,7 +141,8 @@ export capsB="=ep|cap_chown|cap_former|cap_setfcap|cap_dac_override|cap_dac_read
 
 # export sidVB='peass{SUIDVB1_HERE}'
 # export sidVB2='peass{SUIDVB2_HERE}'
-
+#1/08
+profiledG="01-locale-fix.sh|256term.csh|256term.sh|abrt-console-notification.sh|appmenu-qt5.sh|apps-bin-path.sh|bash_completion.sh|cedilla-portuguese.sh|colorgrep.csh|colorgrep.sh|colorls.csh|colorls.sh|colorxzgrep.csh|colorxzgrep.sh|colorzgrep.csh|colorzgrep.sh|csh.local|cursor.sh|gawk.csh|gawk.sh|im-config_wayland.sh|kali.sh|lang.csh|lang.sh|less.csh|less.sh|flatpak.sh|sh.local|vim.csh|vim.sh|vte.csh|vte-2.91.sh|which2.csh|which2.sh|xauthority.sh|Z97-byobu.sh|xdg_dirs_desktop_session.sh|Z99-cloudinit-warnings.sh|Z99-cloud-locale-test.sh"
 
 
 #start with " /", end with "$", divide path and vulnversion "%". SPACE IS ONLY ALLOWED AT BEGINNING, DONT USE IT IN VULN DESCRIPTION
