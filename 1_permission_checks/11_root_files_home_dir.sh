f11cccac1056c8b80e32884911723259dbd12faa
masik <etoyadadaa@gmail.com>
2025-11-21 18:00:14 +0300

    11 compiled

--- 1_permission_checks/11_root_files_home_dir.sh
+++ 1_permission_checks/11_root_files_home_dir.sh
@@ -3,11 +3,15 @@
 print_2title "Searching root owned files in homes up to 40"
 
 found_files=$(find -type f $HOMESEARCH -user root 2>/dev/null)
+echo "$found_files"
 
 found_dirs=$(find -type d $HOMESEARCH -user root 2>/dev/null)
+echo "$found_dirs"
 
 for f in $found_files; do
     ls -la $f | head -n 40 | sed -${E} "s,$sh_usrs,${SED_LIGHT_CYAN},g" | sed "s,$USER,${SED_RED},g"
+    echo "split"
+    ls -la "$f" | head -n 40 | sed -${E} "s,$sh_usrs,${SED_LIGHT_CYAN},g" | sed "s,$USER,${SED_RED},g"
 done;
 
 for d in $found_dirs; do
