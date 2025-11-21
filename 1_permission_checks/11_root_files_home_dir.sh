25b0aa18bdfc6dfd0033116a246cdad6bc75ac94
masik <etoyadadaa@gmail.com>
2025-11-21 17:52:42 +0300

    11 trying

--- 1_permission_checks/11_root_files_home_dir.sh
+++ 1_permission_checks/11_root_files_home_dir.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-print_2title "Searching root owned files in homes up to 30"
+print_2title "Searching root owned files in homes up to 40"
 
-(find $HOMESEARCH -user root 2>/dev/null | head -n 30 | sed -${E} "s,$sh_usrs,${SED_LIGHT_CYAN},g" | sed "s,$USER,${SED_RED},g") \
-|| echo_not_found
+found_files=$(find -type f $HOMESEARCH -user root 2>/dev/null)
+
+found_dirs=$(find -type d $HOMESEARCH -user root 2>/dev/null)
+
+for f in $found_files; do
+    ll $f | head -n 40 | sed -${E} "s,$sh_usrs,${SED_LIGHT_CYAN},g" | sed "s,$USER,${SED_RED},g"
+
+for d in $found_dirs; do
+    echo "$d" | head -n 40 | sed -${E} "s,$sh_usrs,${SED_LIGHT_CYAN},g" | sed "s,$USER,${SED_RED},g"
+#  "found_files" | head -n 40 | sed -${E} "s,$sh_usrs,${SED_LIGHT_CYAN},g" | sed "s,$USER,${SED_RED},g"
+
+
+
 echo ""
