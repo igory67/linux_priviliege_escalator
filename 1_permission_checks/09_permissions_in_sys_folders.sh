29f09a708503f3d8592458169a1c803fe4476f2a
masik <etoyadadaa@gmail.com>
2025-11-21 17:02:21 +0300

    09 critial

--- 1_permission_checks/09_permissions_in_sys_folders.sh
+++ 1_permission_checks/09_permissions_in_sys_folders.sh
@@ -3,13 +3,13 @@
 print_2title "Permissions in init, init.d, systemd, and rc.d"
 
 if ! [ "$IAMROOT" ]; then 
-  check_critial_root_path "/etc/init/"
-  check_critial_root_path "/etc/init.d/"
-  check_critial_root_path "/etc/rc.d/init.d"
-  check_critial_root_path "/usr/local/etc/rc.d"
-  check_critial_root_path "/etc/rc.d"
-  check_critial_root_path "/etc/systemd/"
-  check_critial_root_path "/lib/systemd/"
+  check_critical_root_path "/etc/init/"
+  check_critical_root_path "/etc/init.d/"
+  check_critical_root_path "/etc/rc.d/init.d"
+  check_critical_root_path "/usr/local/etc/rc.d"
+  check_critical_root_path "/etc/rc.d"
+  check_critical_root_path "/etc/systemd/"
+  check_critical_root_path "/lib/systemd/"
 fi
 
 echo ""
