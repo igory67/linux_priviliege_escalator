99dda0adfb3f5f0d3a9445d44bdd5fe312d8c490
masik <etoyadadaa@gmail.com>
2025-11-28 14:30:09 +0300

    13

---
+++ 1_permission_checks/13_root_owned_readable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+print_2title "Readable files belonging to root and readable by me but not world readable"
+
+if ! [ "$IAMROOT" ]; then
+  (find $ROOT_FOLDER -type f -user root ! -perm -o=r ! -path "/proc/*" 2>/dev/null \
+  | grep -v "\.journal" \
+  | while read f; do if [ -r "$f" ]; then ls -l "$f" 2>/dev/null | sed -${E} "s,/.*,${SED_RED},"; fi; done) || echo_not_found
+  echo ""
+else
+    small_print "i am root so idc"
+fi
