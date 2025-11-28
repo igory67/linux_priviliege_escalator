834c3d8cc6e1aa8633baf180d1a2f5a74ca582eb
masik <etoyadadaa@gmail.com>
2025-11-28 14:46:20 +0300

    13 final ig

--- 1_permission_checks/13_root_owned_readable.sh
+++ 1_permission_checks/13_root_owned_readable.sh
@@ -10,7 +10,11 @@ if ! [ "$IAMROOT" ]; then
             ls -la "$f" 2>/dev/null; 
         fi; 
     done)
-    print_red "$find_res"
+    if [ "$find_res"]
+        print_red "$find_res"
+    else 
+        echo_not_found "readable files"
+    # print_red "$find_res"
 else
     small_print "i am root so idc"
 fi
@@ -19,14 +23,17 @@ echo ""
 print_2title "Writable files belonging to root and writable by me but not world writable"
 
 if ! [ "$IAMROOT" ]; then
-  find_res=$(find $ROOT_FOLDER -type f -user root ! -perm -o=w ! -path "/proc/*" 2>/dev/null \
+  find_res_w=$(find $ROOT_FOLDER -type f -user root ! -perm -o=w ! -path "/proc/*" 2>/dev/null \
   | grep -v "\.journal" \
   | while read f; do
         if [ -w "$f" ]; then
             ls -la "$f" 2>/dev/null; 
         fi; 
     done)
-    print_red "$find_res"
+    if [ "$find_res_w"]
+        print_red "$find_res_w"
+    else 
+        echo_not_found "writable files"
 else
     small_print "i am root so idc"
 fi
