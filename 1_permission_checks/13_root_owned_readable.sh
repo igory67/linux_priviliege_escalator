900d04469921c36f253cbc8c2b7c16e3443d3aea
masik <etoyadadaa@gmail.com>
2025-11-28 14:36:26 +0300

    13 working

--- 1_permission_checks/13_root_owned_readable.sh
+++ 1_permission_checks/13_root_owned_readable.sh
@@ -6,7 +6,16 @@ if ! [ "$IAMROOT" ]; then
   (find $ROOT_FOLDER -type f -user root ! -perm -o=r ! -path "/proc/*" 2>/dev/null \
   | grep -v "\.journal" \
   | while read f; do if [ -r "$f" ]; then ls -l "$f" 2>/dev/null | sed -${E} "s,/.*,${SED_RED},"; fi; done) || echo_not_found
-  echo ""
 else
     small_print "i am root so idc"
 fi
+echo ""
+
+find_res=$(find $ROOT_FOLDER -type f -user root ! -perm -o=r ! -path "/proc/*" 2>/dev/null \
+  | grep -v "\.journal" \
+  | while read f; do
+        if [ -r "$f" ]; then
+            ls -l "$f" 2>/dev/null; 
+        fi; 
+    done)
+print_red "$find_res"
