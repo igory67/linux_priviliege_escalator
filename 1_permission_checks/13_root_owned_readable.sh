95b56efa610df5af643e6c9dafaf754c81af6318
masik <etoyadadaa@gmail.com>
2025-11-28 14:41:14 +0300

    13 working

--- 1_permission_checks/13_root_owned_readable.sh
+++ 1_permission_checks/13_root_owned_readable.sh
@@ -3,19 +3,31 @@
 print_2title "Readable files belonging to root and readable by me but not world readable"
 
 if ! [ "$IAMROOT" ]; then
-  (find $ROOT_FOLDER -type f -user root ! -perm -o=r ! -path "/proc/*" 2>/dev/null \
+  find_res=$(find $ROOT_FOLDER -type f -user root ! -perm -o=r ! -path "/proc/*" 2>/dev/null \
   | grep -v "\.journal" \
-  | while read f; do if [ -r "$f" ]; then ls -l "$f" 2>/dev/null | sed -${E} "s,/.*,${SED_RED},"; fi; done) || echo_not_found
+  | while read f; do
+        if [ -r "$f" ]; then
+            ls -la "$f" 2>/dev/null; 
+        fi; 
+    done)
+    print_red "$find_res"
 else
     small_print "i am root so idc"
 fi
 echo ""
 
-find_res=$(find $ROOT_FOLDER -type f -user root ! -perm -o=r ! -path "/proc/*" 2>/dev/null \
+print_2title "Writable files belonging to root and writable by me but not world writable"
+
+if ! [ "$IAMROOT" ]; then
+  find_res=$(find $ROOT_FOLDER -type f -user root ! -perm -o=w ! -path "/proc/*" 2>/dev/null \
   | grep -v "\.journal" \
   | while read f; do
-        if [ -r "$f" ]; then
-            ls -l "$f" 2>/dev/null; 
+        if [ -w "$f" ]; then
+            ls -la "$f" 2>/dev/null; 
         fi; 
     done)
-print_red "$find_res"
+    print_red "$find_res"
+else
+    small_print "i am root so idc"
+fi
+echo ""
