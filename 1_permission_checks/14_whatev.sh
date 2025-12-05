b193ac29c2d008de7994f8748f7a39f2b9b471a1
igory67 <serveruser1@mydomain.local>
2025-12-05 09:00:21 +0000

    14

--- 1_permission_checks/14_whatev.sh
+++ 1_permission_checks/14_whatev.sh
@@ -9,23 +9,23 @@ if ! [ "$IAMROOT" ]; then
   | grep -Ev "$notExtensions" \
   | sort \
   | uniq \
+  | awk -F/ '{line_init=$0; if (!cont){ cont=0 }; $NF=""; act=$0; if (act == | pre){(cont += 1)} else {cont=0}; if (cont < 5){ print line_init; } if (cont ==  | "5"){print "#)You_can_write_even_more_files_inside_last_directory\n"}; pre=act }'\
 | head -n 200)
-  #| awk -F/ '{line_init=$0; if (!cont){ cont=0 }; $NF=""; act=$0; if (act == 
-  #| pre){(cont += 1)} else {cont=0}; if (cont < 5){ print line_init; } if (cont == 
-  #| "5"){print "#)You_can_write_even_more_files_inside_last_directory\n"}; pre=act }'\
-#| head -n 200)
 #подумать про это авк, пока странно кажется
 #  | head -n 200)
 	small_print "$obmowbe"
-  printf "%s\n" "$obmowbe" | while read l; do
+
+  printf "%s\n" "$obmowbe" | while read l; 
+	do
+
     if echo "$l" | grep -q "You_can_write_even_more_files_inside_last_directory"; then
       # printf $ITALIC"$l\n"$NC;
       small_print "$l";
     elif echo "$l" | grep -qE "$writeVB"; then
-      print_red_yellow "$1"
+      print_red_yellow "$l"
       # echo "$l" | sed -${E} "s,$writeVB,${SED_RED_YELLOW},"
     else 
-      print_red "$1"
+      print_red "$l"
       # echo "$l" | sed -${E} "s,$writeB,${SED_RED},"
     fi
   done
