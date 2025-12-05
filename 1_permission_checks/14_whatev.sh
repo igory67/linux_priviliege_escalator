ff2789312692862d4c533f359c84603e839c64c7
igory67 <serveruser1@mydomain.local>
2025-12-05 14:25:29 +0000

    14 close

--- 1_permission_checks/14_whatev.sh
+++ 1_permission_checks/14_whatev.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+echo "14"
 print_2title "THIS IS 1ST"
 if ! [ "$IAMROOT" ]; then
   print_2title "Interesting writable files owned by me or writable by everyone (not in Home) (max 200)"
+#echo "1414141"
   #In the next file, you need to specify type "d" and "f" to avoid fake link files apparently writable by all
   #ombowbe это название болезни автора (owned by me or writable by everyone)
   obmowbe=$(find $ROOT_FOLDER '(' -type f -or -type d ')' '(' '(' -user $USER ')' -or '(' -perm -o=w ')' ')' ! -path "/proc/*" ! -path "/sys/*" ! -path "$HOME/*" 2>/dev/null \
@@ -10,8 +11,8 @@ if ! [ "$IAMROOT" ]; then
   | sort \
   | uniq \
 | head -n 200)
-  | awk -F/ '{line_init=$0;
-   if (!cont){ cont=0 }; $NF=""; act=$0; if (act == | pre){(cont += 1)} else {cont=0}; if (cont < 5){ print line_init; } if (cont ==  | "5"){print "#)You_can_write_even_more_files_inside_last_directory\n"}; pre=act }'\
+#  | awk -F/ '{line_init=$0;
+#   if (!cont){ cont=0 }; $NF=""; act=$0; if (act == | pre){(cont += 1)} else {cont=0}; if (cont < 5){ print line_init; } if (cont ==  | "5"){print "#)You_can_write_even_more_files_inside_last_directory\n"}; pre=act }'\
 #  подумать про это авк, пока странно кажется
 #  | head -n 200)
 	# small_print "$obmowbe"
--- 1_permission_checks/test.sh
+++
@@ -1,8 +0,0 @@
-echo -e "/etc/a/file1\n/etc/a/file2\n/etc/a/file3\n/etc/a/file4\n/etc/a/file5\n/etc/a/file6\n/etc/b/file1" | \
-awk -F/ '{
-    dir=$0; 
-    sub(/\/[^\/]+$/, "", dir);  
-    count[dir]++
-    if (count[dir] <= 5) print $0
-    else if (count[dir] == 6) print "# More in: " dir
-}'
