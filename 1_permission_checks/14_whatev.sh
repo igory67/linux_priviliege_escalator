24701d27fe86c54287bcbbd401be9c74ba0fa538
igory67 <serveruser1@mydomain.local>
2025-12-05 08:25:02 +0000

    14 in pr

--- 1_permission_checks/14_whatev.sh
+++ 1_permission_checks/14_whatev.sh
@@ -1,4 +1,6 @@
-print_2_title "THIS IS 1ST"
+#!/bin/bash
+
+print_2title "THIS IS 1ST"
 if ! [ "$IAMROOT" ]; then
   print_2title "Interesting writable files owned by me or writable by everyone (not in Home) (max 200)"
   #In the next file, you need to specify type "d" and "f" to avoid fake link files apparently writable by all
@@ -7,10 +9,14 @@ if ! [ "$IAMROOT" ]; then
   | grep -Ev "$notExtensions" \
   | sort \
   | uniq \
-  #| awk -F/ '{line_init=$0; if (!cont){ cont=0 }; $NF=""; act=$0; if (act == pre){(cont += 1)} else {cont=0}; if (cont < 5){ print line_init; } if (cont == "5"){print "#)You_can_write_even_more_files_inside_last_directory\n"}; pre=act }' \
+| head -n 200)
+  #| awk -F/ '{line_init=$0; if (!cont){ cont=0 }; $NF=""; act=$0; if (act == 
+  #| pre){(cont += 1)} else {cont=0}; if (cont < 5){ print line_init; } if (cont == 
+  #| "5"){print "#)You_can_write_even_more_files_inside_last_directory\n"}; pre=act }'\
+#| head -n 200)
 #подумать про это авк, пока странно кажется
-  | head -n 200)
-
+#  | head -n 200)
+	small_print "$obmowbe"
   printf "%s\n" "$obmowbe" | while read l; do
     if echo "$l" | grep -q "You_can_write_even_more_files_inside_last_directory"; then
       # printf $ITALIC"$l\n"$NC;
