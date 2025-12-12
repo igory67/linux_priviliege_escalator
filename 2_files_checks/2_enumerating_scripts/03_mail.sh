7e74a77c81aff721d8cb10ee863a17f130df8080
masik <etoyadadaa@gmail.com>
2025-12-12 16:20:01 +0300

    mail fix sed

--- 2_files_checks/2_enumerating_scripts/03_mail.sh
+++ 2_files_checks/2_enumerating_scripts/03_mail.sh
@@ -12,11 +12,11 @@ print_2title "CHECKING MAILS"
 #(find /var/mail/ /var/spool/mail/ /private/var/mail -type f -ls 2>/dev/null | head -n 50 | sed "s,$USER,${SED_RED},g" | sed -${E} "s,$sh_usrs,${SED_RED}," | sed -${E} "s,$nosh_usrs,${SED_BLUE},g" | sed -${E} "s,$knw_usrs,${SED_GREEN},g" | sed "s,root,${SED_GREEN},g" ) || echo_not_found
 
 (find /var/mail/ /var/spool/mail/ /private/var/mail -type f -ls 2>/dev/null | head -n 50 | \
-  sed -${E} "s|$(printf '%s' "$USER" | sed 's/[^^]/[&]/g; s/\^/\\^/g')|${SED_RED}|g" | \
-  sed -${E} "s|root|${SED_GREEN}|g" | \
-  sed -${E} "s|${sh_usrs}|${SED_RED}|g" | \
-  sed -${E} "s|${nosh_usrs}|${SED_BLUE}|g" | \
-  sed -${E} "s|${knw_usrs}|${SED_GREEN}|g"
+  sed -${E} "s,$(printf '%s' "$USER" | sed 's/[^^]/[&]/g; s/\^/\\^/g'),${SED_RED},g" | \
+  sed -${E} "s,root,${SED_GREEN},g" | \
+  sed -${E} "s,${sh_usrs},${SED_RED},g" | \
+  sed -${E} "s,${nosh_usrs},${SED_BLUE},g" | \
+  sed -${E} "s,${knw_usrs},${SED_GREEN},g"
 ) || echo_not_found
 
 echo ""
