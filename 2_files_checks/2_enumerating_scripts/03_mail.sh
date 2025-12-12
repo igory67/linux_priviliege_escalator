77c7437c8f5057c109a94f4c0cb4365627438693
masik <etoyadadaa@gmail.com>
2025-12-12 16:17:27 +0300

    mail fix sed

--- 2_files_checks/2_enumerating_scripts/03_mail.sh
+++ 2_files_checks/2_enumerating_scripts/03_mail.sh
@@ -9,5 +9,5 @@ echo ""
 
 
 print_2title "CHECKING MAILS"
-(find /var/mail/ /var/spool/mail/ /private/var/mail -type f -ls 2>/dev/null | head -n 50 | sed -${E} "s,$sh_usrs,${SED_RED}," | sed -${E} "s,$nosh_usrs,${SED_BLUE},g" | sed -${E} "s,$knw_usrs,${SED_GREEN},g" | sed "s,root,${SED_GREEN},g" | sed "s,$USER,${SED_RED},g") || echo_not_found
+(find /var/mail/ /var/spool/mail/ /private/var/mail -type f -ls 2>/dev/null | head -n 50 | sed "s,$USER,${SED_RED},g" | sed -${E} "s,$sh_usrs,${SED_RED}," | sed -${E} "s,$nosh_usrs,${SED_BLUE},g" | sed -${E} "s,$knw_usrs,${SED_GREEN},g" | sed "s,root,${SED_GREEN},g" ) || echo_not_found
 echo ""
