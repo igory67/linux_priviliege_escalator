d75374de9d38b731072c4a0cdbcd959fe911fdd7
igory67 <serveruser1@mydomain.local>
2025-12-23 11:40:56 +0000

    put tsetsetsetestestsetsetset

---
+++ tsesetestse.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+sna=$(ls -a -l)
+
+#print_red "aaaa"
+#print_red "$sna"
+#print_red "ass %s", $sna
+#print_red "adasfdsa" "$sna"
+#print_red "adasfdsa %s", "$sna"
+
+print_green "asdfaksdfasd"
+print_blue "ssss \n $sna \n ssss"
