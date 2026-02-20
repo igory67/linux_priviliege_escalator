8ede6f000c84e0f957d56086656415a2bd99d755
igory67 <etoyadadaa@gmail.com>
2026-02-20 16:07:03 +0300

    suid_sudo

--- 1_permission_checks/2_enumerating_checks_again/16_suid_sudo.sh
+++ 1_permission_checks/2_enumerating_checks_again/16_suid_sudo.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-print_2title "sudo -l and /etc/sudoers |.d"
+print_2title "sudo -l and /etc/sudoers | sudoers.d"
 
-(echo '' \
+
+read -s -p "Enter sudo password: " PASSWORD
+echo ""  # Newline after hidden input
+(echo "$PASSWORD" \
 | timeout 1 sudo -S -l \
 | sed "s,_proxy,${SED_RED},g" \
 | sed "s,$sudoG,${SED_GREEN},g" \
@@ -11,17 +14,7 @@ print_2title "sudo -l and /etc/sudoers |.d"
 | sed "s,\!root,${SED_RED},") 2>/dev/null \
 || echo_not_found "sudo"
 
-  
-if [ "$PASSWORD" ]; 
-then
-  (echo "$PASSWORD" \
-  | timeout 1 sudo -S -l \
-  | sed "s,_proxy,${SED_RED},g" \
-  | sed "s,$sudoG,${SED_GREEN},g" \
-  | sed -${E} "s,$sudoVB,${SED_RED_YELLOW}," \
-  | sed -${E} "s,$sudoB,${SED_RED},g") 2>/dev/null  \
-  || echo_not_found "sudo"
-fi
+unset $PASSWORD
 
 ( grep -Iv "^$" cat /etc/sudoers \
 | grep -v "#" \
@@ -32,11 +25,13 @@ fi
 | sed "s,pwfeedback,${SED_RED},g" ) 2>/dev/null  \
 || echo_not_found "/etc/sudoers"
 
+#вдруг можем писать в судоерс лол
 if ! [ "$IAMROOT" ] && [ -w '/etc/sudoers.d/' ]; 
 then
   echo "You can create a file in /etc/sudoers.d/ and escalate privileges" | sed -${E} "s,.*,${SED_RED_YELLOW},"
 fi
 
+#вдруг можем читать что-то в судоерс
 for f in /etc/sudoers.d/*; do
   if [ -r "$f" ]; then
     echo "Sudoers file: $f is readable" | sed -${E} "s,.*,${SED_RED},g"
